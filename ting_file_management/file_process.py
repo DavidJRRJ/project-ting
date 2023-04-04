bdadc3e336e4e8f6268f3499166db40c73c55ee1
David Junior <david.lourenco94@gmail.com>
2023-04-04 17:26:45 -0300

    req03: implementação da função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    instance_length = len(instance)
+    index = 0
+    while index < instance_length:
+        data_dict = instance.search(index)
+        if data_dict["nome_do_arquivo"] == path_file:
+            return
+        index += 1
+    lines_list = txt_importer(path_file)
+    file_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines_list),
+        "linhas_do_arquivo": lines_list
+    }
+    instance.enqueue(file_dict)
+    sys.stdout.write(str(file_dict))
 
 
 def remove(instance):
