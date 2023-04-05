eb9076649a403715949892836e7690192384c6a3
David Junior <david.lourenco94@gmail.com>
2023-04-04 21:39:16 -0300

    req08: implementação da função search_by_world

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,18 +1,42 @@
 def exists_word(word, instance):
-    instance_length = len(instance)
-    count_list = []
-    for index in range(instance_length):
-        current_dict = instance.search(index)
-        file_lines = current_dict["linhas_do_arquivo"]
-        info = {"palavra": word,
-                "arquivo": current_dict["nome_do_arquivo"], "ocorrencias": []}
-        for line_index, line in enumerate(file_lines):
-            if word.lower() in line.lower():
-                info["ocorrencias"].append({"linha": line_index + 1})
-    if info["ocorrencias"]:
-        count_list.append(info)
+    count_list = [
+        info for index in range(len(instance))
+        if (info := instance.search(index))
+        and (file_lines := info["linhas_do_arquivo"])
+        and (
+            info := {
+                "palavra": word,
+                "arquivo": info["nome_do_arquivo"],
+                "ocorrencias": [
+                    {"linha": line_index + 1}
+                    for line_index, line in enumerate(file_lines)
+                    if word.lower() in line.lower()
+                ],
+            }
+        )
+        and info["ocorrencias"]
+    ]
     return count_list
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    count_list = [
+        {
+            "palavra": word,
+            "arquivo": instance.search(index)["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": line_index + 1, "conteudo": line}
+                for line_index, line in enumerate(
+                    instance.search(index)["linhas_do_arquivo"]
+                )
+                if line.lower().count(word.lower()) > 0
+            ],
+        }
+        for index in range(len(instance))
+        if any(
+            line.lower().count(word.lower()) > 0
+            for line in instance.search(index)["linhas_do_arquivo"]
+        )
+    ]
+
+    return count_list
