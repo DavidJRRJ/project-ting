bdebac6075b7fe4f767ec838f7627f95988d7c9e
David Junior <david.lourenco94@gmail.com>
2023-04-04 21:06:35 -0300

    req07: implementação da função exist_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,17 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    instance_length = len(instance)
+    count_list = []
+    for index in range(instance_length):
+        current_dict = instance.search(index)
+        file_lines = current_dict["linhas_do_arquivo"]
+        info = {"palavra": word,
+                "arquivo": current_dict["nome_do_arquivo"], "ocorrencias": []}
+        for line_index, line in enumerate(file_lines):
+            if word.lower() in line.lower():
+                info["ocorrencias"].append({"linha": line_index + 1})
+    if info["ocorrencias"]:
+        count_list.append(info)
+    return count_list
 
 
 def search_by_word(word, instance):
